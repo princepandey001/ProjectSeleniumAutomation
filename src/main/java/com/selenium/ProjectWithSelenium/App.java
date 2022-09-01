57e96d04137cf25c9868d84941d123eccb73a3fd
princepandey001 <pp001official@gmail.com>
2022-09-01 16:31:27 +0530

    Initial commit for add to cart product 1(watch)

---
+++ src/main/java/com/selenium/ProjectWithSelenium/App.java
@@ -0,0 +1,28 @@
+package com.selenium.ProjectWithSelenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+		// Maximize chrome window
+		driver.manage().window().maximize();
+
+		// Navigate to amazon website
+		driver.get("https://amazon.in");
+
+		// Add to cart a "watch" with the help of method of product1 class
+		product1.getWatch(driver);
+
+	}
+
+}
